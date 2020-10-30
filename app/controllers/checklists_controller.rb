473943cbf6da3d11059219137f4c2a922ac47493
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-10-30 12:07:29 +0200

    Add checklists_controller with index, create, show, delete actions. In create action we can create a checklist with title and description, but without questions.

---
+++ app/controllers/checklists_controller.rb
@@ -0,0 +1,40 @@
+class ChecklistsController < ApplicationController
+    before_action :set_checklist, only: [:show, :destroy]
+
+    def index
+        @checklists = Checklist.all
+    end
+
+    def new 
+        @checklist = Checklist.new
+    end
+
+    def create 
+        @checklist = Checklist.new(checklist_params)
+        if @checklist.save
+            flash[:notice] = "Checklist was created"
+            redirect_to checklists_path
+        else
+            render 'new'
+        end
+    end
+    
+    def show
+    end
+
+    def destroy
+        @checklist.destroy
+        flash[:notice] = "Checklist was delete"
+        redirect_to checklists_path
+    end
+
+    private
+
+        def checklist_params
+            params.require(:checklist).permit(:title, :description)
+        end
+
+        def set_checklist
+            @checklist = Checklist.find(params[:id])
+        end
+end
