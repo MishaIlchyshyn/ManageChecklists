ff280fbb91ce388a5dffa111a971c478b4c91710
MishaIlchyshyn <ilchyshynmuchailo@gmail.com>
2020-11-10 16:34:46 +0200

    Create modal window to fill-checklist

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get "checklists/fill_checklist" => 'checklists#fill_checklist', :as => :fill_checklist
+  
   devise_for :users
+
   root "pages#index"
 
   resources :audits
